4ac92c36c2be0412c1ea6aefc89adb4b9fca4ccb
Craig McNamara <cmcnamara87@gmail.com>
2016-02-19 14:25:21 +1000

    fix(activities page): only show timetables for the future, not the past

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -15,7 +15,7 @@ class ActivitiesController extends Controller
     public function index()
     {
         $sunday = Carbon::parse('Saturday');
-        if(Carbon::today()->isSunday()) {
+        if (Carbon::today()->isSunday()) {
             $saturday = Carbon::parse('Last Saturday');
         } else {
             $saturday = Carbon::parse('Saturday');
@@ -24,10 +24,14 @@ class ActivitiesController extends Controller
         $activities = Activity::all();
 
         $timetableIds = Timetable::where('start_time', '>=', Carbon::now())->where('start_time', '<', Carbon::today()->endOfDay())->lists('activity_id');
-        $todaysActivites = Activity::whereIn('id', $timetableIds)->get();
+        $todaysActivites = Activity::whereIn('id', $timetableIds)->with(array('timetables' => function ($q) {
+            $q->where('start_time', '>=', Carbon::now());
+        }))->get();
 
         $timetableIds = Timetable::where('start_time', '>=', $saturday)->where('start_time', '<', $sunday->endOfDay())->lists('activity_id');
-        $thisWeekendsActivites = Activity::whereIn('id', $timetableIds)->get();
+        $thisWeekendsActivites = Activity::whereIn('id', $timetableIds)->with(array('timetables' => function ($q) {
+            $q->where('start_time', '>=', Carbon::now());
+        }))->get();
 
         // load the view and pass the nerds
         return view('activities.index', compact('activities', 'todaysActivites', 'thisWeekendsActivites'));
