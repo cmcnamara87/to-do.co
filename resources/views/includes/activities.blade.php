1f3238c9248daa0a732346ef002d35910bb41b23
Craig McNamara <cmcnamara87@gmail.com>
2016-03-13 15:47:00 +1000

    add stuff

--- resources/views/includes/activities.blade.php
+++ resources/views/includes/activities.blade.php
@@ -11,21 +11,29 @@
       rgba(0, 0, 0, 0.6)
     ),
     url({{ asset($activity->image_url) }});">
-                        <p style="color: white;font-weight: bold;opacity: 0.8;;">
-                            @if($activity->price == 0)
-                                Free
-                            @elseif($activity->price > 0)
-                                ${{ $activity->price }}
-                                @if($activity->price != $activity->value)
-                                    was ${{ $activity->value }}
+                        <div>
+                            <p class="text-capitalize">
+                                {{ $activity->categories->first()->name }}
+                            </p>
+                            <p style="color: white;font-weight: bold;opacity: 0.8;;">
+                                @if($activity->price == 0)
+                                    Free
+                                @elseif($activity->price > 0)
+                                    ${{ $activity->price }}
+                                    @if($activity->price != $activity->value)
+                                        was ${{ $activity->value }}
+                                    @endif
                                 @endif
-                            @endif
-                        </p>
+                            </p>
+                        </div>
+
                         <h3 style="margin-top:100px;font-size:20px;"><a style="color:inherit;" href="{{ url("{$activity->slug}") }}">{{ $activity->title }}</a></h3>
                         <p style="opacity: 0.9;margin-bottom: 15px;">
                             {{ strip_tags($activity->description) }}
                         </p>
-                        <button style="padding:8px 25px;margin-bottom: 20px;" class="btn btn-warning">View Activity</button>
+
+                        <button style="padding:8px 25px;margin-bottom: 20px;" class="btn btn-info">View Activity</button>
+
                     </div>
                 </div>
             </div>
