6a3d5861fdf736998dc6f9ef2f29cb3e8af8f447
Craig McNamara <cmcnamara87@gmail.com>
2016-03-14 17:40:24 +1000

    feat(activity): change style again

--- resources/views/includes/activities.blade.php
+++ resources/views/includes/activities.blade.php
@@ -1,8 +1,8 @@
 @foreach (array_chunk($activities->all(), 4) as $activitiesRow)
-    <div class="row">
+    <div class="row" style="margin-bottom: 30px;">
         @foreach ($activitiesRow as $activity)
             <div class="col-sm-3">
-                <div class="panel panel-default">
+                {{--<div class="panel panel-default">--}}
                     <div>
                         @if(isset($activity->score))
                             <strong>{{ $activity->score }}</strong>
@@ -11,26 +11,28 @@
                             <img src="{{ asset($activity->image_url) }}" alt="{{ $activity->title }}" style="width:100%;"/>
                         </a>
                     </div>
-                    <div class="panel-body">
-                        <h3><a href="{{ url("activities/{$activity->slug}") }}">{{ $activity->title }}</a></h3>
-                        <h4>
+                    {{--<div class="panel-body">--}}
+                        <h5>
+                            <a href="{{ url("activities/{$activity->slug}") }}">{{ $activity->title }}</a>
+                        </h5>
+                        <p class="text-muted">
                             @if($activity->price == 0)
                                 Free
                             @elseif($activity->price > 0)
                                 ${{ $activity->price }}
                                 @if($activity->price != $activity->value)
-                                was ${{ $activity->value }}
+                                    was ${{ $activity->value }}
                                 @endif
                             @endif
-                        </h4>
+                        </p>
                         {{--<ul>--}}
                             {{--@foreach($activity->categories as $category)--}}
                                 {{--<li>{{ $category->name }}</li>--}}
                             {{--@endforeach--}}
                         {{--</ul>--}}
 
-                    </div>
-                </div>
+                    {{--</div>--}}
+                {{--</div>--}}
             </div>
         @endforeach
     </div>
