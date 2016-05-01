542ea93785fa81a4939e3e8e57d303ca41d49423
Craig McNamara <cmcnamara87@gmail.com>
2016-05-01 22:16:52 +1000

    feat(activities): simply styling to something really simple

--- resources/views/features/index.blade.php
+++ resources/views/features/index.blade.php
@@ -3,15 +3,16 @@
 @section('description', 'Find Great Activities and Events from around Brisbane')
 @section('content')
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1>10 Cools Things To Do in Brisbane Every Day</h1>
-            <p>Find Great Activities and Events from around Brisbane</p>
-        </div>
-    </div>
+    {{--<div class="jumbotron">--}}
+        {{--<div class="container">--}}
+            {{----}}
+            {{--<p>Find Great Activities and Events from around Brisbane</p>--}}
+        {{--</div>--}}
+    {{--</div>--}}
     <div class="container">
+        <h1>10 Cools Things To Do in Brisbane Every Day</h1>
         @foreach($features as $feature)
-            <h3>{{ $feature->date->format('l') }}</h3>
+            <h3 style="margin-bottom: 20px;margin-top:40px;">{{ $feature->date->format('l') }}</h3>
             @include('includes.feature', ["feature" => $feature])
         @endforeach
     </div>
--- resources/views/includes/feature.blade.php
+++ resources/views/includes/feature.blade.php
@@ -1,13 +1,13 @@
 
-<div class="row">
-    <div class="col-sm-8 ">
-
-        <ul class="list-group">
+<div>
+    <div>
+        <ul class="list-unstyled">
             @foreach($feature->activities as $activity)
-            <li class="list-group-item" @if(\Carbon\Carbon::now()->gte($activity->timetables->last()->end_time)) style="opacity:0.5;text-decoration:line-through;" @endif>
-                <div class="media">
+            <li @if(\Carbon\Carbon::now()->gte($activity->timetables->last()->end_time)) style="opacity:0.5;text-decoration:line-through;" @endif>
+                <div class="media" style="border-bottom: 1px solid #eee;margin-bottom: 20px;padding-bottom: 20px;">
                     <div class="pull-left">
-                        <img src="{{ asset($activity->image_url) }}" alt="{{ $activity->title }}" style="width:100px;"/>
+                        <img src="{{ asset($activity->image_url) }}" alt="{{ $activity->title }}" style="width:50px;height:50px;"
+                                class="img-circle"/>
                     </div>
                     <div class="media-body">
                         <ul class="pull-right">
@@ -16,20 +16,10 @@
                             @endforeach
                         </ul>
 
-                        <h4><a href="{{ url("activities/{$activity->slug}") }}">{{ $activity->title }}</a></h4>
-                        @if($activity->price > 0)
-                        <p class="text-muted">
-                            ${{ $activity->price }}
-                        </p>
-                        @endif
-                        @if($activity->price == 0)
-                        <p class="text-muted">
-                            Free
-                        </p>
-                        @endif
+                        <h4 style="margin:0;"><a style="color:#333;font-size:16px" href="{{ url("activities/{$activity->slug}") }}">{{ $activity->title }}</a></h4>
 
                         @foreach($activity->timetables->take(1) as $timetable)
-                            <p class="text-muted">
+                            <p class="text-muted" style="margin:0;">
                                 @if($timetable->start_time->day == $timetable->end_time->day)
                                     <strong>{{ $timetable->start_time->format('l j F Y') }}</strong>
                                     {{ $timetable->start_time->format('h:i A') }} -
@@ -43,9 +33,20 @@
                             </p>
                         @endforeach
 
-                        <p>
-                            {!! str_limit(strip_tags($activity->description), 150, '...') !!}
-                        </p>
+                        @if($activity->price > 0)
+                            <p class="text-muted" style="margin:0;">
+                                ${{ $activity->price }}
+                            </p>
+                        @endif
+                        @if($activity->price == 0)
+                            <p class="text-muted" style="margin:0;">
+                                Free
+                            </p>
+                        @endif
+
+                        {{--<p>--}}
+                            {{--{!! str_limit(strip_tags($activity->description), 150, '...') !!}--}}
+                        {{--</p>--}}
 
                         <ul>
                             @if(!$activity->timetables->count())
@@ -60,8 +61,8 @@
             </li>
             @endforeach
             @if($feature->activities->count() < 10)
-            <li class="list-group-item">
-                + {{ 10 - $feature->activities->count() }} activities expired.
+            <li>
+               {{ 10 - $feature->activities->count() }} activities expired.
                 {{--<a href="{{ url("activities") }}">See all the events on {{ $feature->date->format('l j F') }}</a>--}}
             </li>
             @endif
